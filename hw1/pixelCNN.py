fixed a bug in masking

hw1/pixelCNN.py
@@ -30,27 +30,30 @@ def get_pixelcnn_mask(kernel_size, in_channels, out_channels, isTypeA, n_channel
 
     Returns masks of shape (kernel_size, kernel_size, # in channels, # out channels)
     """
-    mask = np.ones((kernel_size, kernel_size, n_channels, n_channels))
+    channel_masks = np.ones((kernel_size, kernel_size, n_channels, n_channels))
     centre = kernel_size // 2
     # bottom rows 0s
-    mask[centre+1:, :, :, :] = 0.
+    channel_masks[centre+1:, :, :, :] = 0.
     # right of centre on centre row 0s
-    mask[centre:, centre+1:, :, :] = 0.
+    channel_masks[centre:, centre+1:, :, :] = 0.
     # deal with centre based on mask "way": factorised or full
     # rows are channels in prev layer, columns are channels in this layer
     if factorised:
         if isTypeA:
-            mask[centre, centre, :, :] = 0.
+            channel_masks[centre, centre, :, :] = 0.
     else:
         # centre depends on mask type A or B
         k = 0 if isTypeA else 1
         # reverse i and j to get RGB ordering (other way would be BGR)
         i, j = np.triu_indices(n_channels, k)
-        mask[centre, centre, j, i] = 0.
+        channel_masks[centre, centre, j, i] = 0.
 
+    # we use repeat not tile because this keeps the correct ordering we need
+    tile_shape = (int(np.ceil(in_channels / n_channels)), int(np.ceil(out_channels / n_channels)))
+    masks = np.repeat(channel_masks, tile_shape[0], axis=2)
+    masks = np.repeat(masks, tile_shape[1], axis=3)
     # tile the masks to potentially more than needed, then retrieve the number of channels wanted
-    mask = np.tile(mask, (int(np.ceil(in_channels / n_channels)), int(np.ceil(out_channels / n_channels))))
-    return mask[:, :, :in_channels, :out_channels]
+    return masks[:, :, :in_channels, :out_channels]
 
 
 class MaskedCNN(tf.keras.layers.Conv2D):

hw1/pixelCNN_DS.py
@@ -230,14 +230,14 @@ def compare_masks():
 
 def display_mask_reshape(masks, kernel_size, n, c, i):
     """
-    Displays the ith input channel's masks in C x N grid
+    Displays the ith input channel's masks in N x C grid
     """
     # in channels as batch dimension
     mask = masks[:, :, i]
     # use tf reshape
     mask = np.array(tf.reshape(mask, (kernel_size, kernel_size, c, n)))
     print(mask.shape)
-    mask_flat = np.concatenate(mask.transpose([3, 1, 0, 2]), axis=0).transpose([2, 1, 0])
+    mask_flat = np.concatenate(mask.transpose([2, 1, 0, 3]), axis=0).transpose([2, 1, 0])
     print(mask_flat.shape)
     # data is shape (n, h, w, c)
     # plots n rows of images
@@ -257,7 +257,7 @@ def tf_reshape_masks():
     out_c = C * N
     mask_letter = "A"
     factorised = False
-    i = 0
+    i = 4
     mask = get_pixelcnn_mask(kernel_size, in_c, out_c, mask_letter == "A", C, factorised)
     print("Mask in shape", mask.shape)
     display_mask_reshape(mask, kernel_size, N, C, i)