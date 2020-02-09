b8ce7fa22c84ae2d18a92a1d6a71a8ad78027a96
Ollie <olliejday@gmail.com>
2020-02-09 20:57:01 +0000

    trying my model with np sampling

--- hw1/pixelCNN.py
+++ hw1/pixelCNN.py
@@ -253,7 +253,7 @@ class PixelCNN:
         loss = self.loss(X, logits)
         return loss
 
-    def get_samples(self, n, seed=123):
+    def get_samples_tfp(self, n, seed=123):
         """
         Generation is done from blank image (all 0s), we then sample R channel
         of first pixel, then G then B and then for second pixel etc.
@@ -270,6 +270,24 @@ class PixelCNN:
                     pixel_dist = tfp.distributions.Categorical(probs=model_preds[:, h, w, c])
                     images[:, h, w, c] = pixel_dist.sample(1, seed=seed)
         return images
+
+    def get_samples(self, n, seed=123):
+        """
+        Generation is done from blank image (all 0s), we then sample R channel
+        of first pixel, then G then B and then for second pixel etc.
+        We batch this for efficiency.
+        """
+        images = np.zeros((n, self.H, self.W, self.C))
+        # start with random values for first channel of first pixel (this is updated in first pass)
+        images[:, 0, 0, 0] = np.random.choice(self.n_vals, n)
+        for h in range(self.H):
+            for w in range(self.W):
+                for c in range(self.C):
+                    model_preds = self.forward_softmax(images)
+                    # categorical over pixel values
+                    for i in range(n):
+                        images[i, h, w, c] = np.random.choice(self.N, p=model_preds[i, h, w, c])
+                return images
 
 
 def plot_image(image, dir_path, title, n_vals=3):
